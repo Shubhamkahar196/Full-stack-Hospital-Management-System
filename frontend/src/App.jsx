a41bba0c47f81a43eb64ad5e69bd857a6dfcae74
Shubham kahar <Shubhamkahar196@gmail.com>
2025-04-18 14:58:59 +0530

    Adding Login and Logout from Frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,56 @@
-import React from 'react'
-import "./App.css"
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
-import Appointment from './pages/Appointment'
-import About from './pages/About'
-import Home from './pages/Home'
-import Register from './pages/Register'
-import Login from './pages/Login'
-
-import { ToastContainer} from 'react-toastify';
-
+import React, { useContext, useEffect } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Appointment from "./pages/Appointment";
+import AboutUs from "./pages/About";
+import Register from "./pages/Register";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+import { Context } from "./main";
+import Login from "./Pages/Login";
 const App = () => {
+  const { isAuthenticated, setIsAuthenticated, setUser } =
+    useContext(Context);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:4000/api/v1/user/patient/me",
+          {
+            withCredentials: true,
+          }
+        );
+        setIsAuthenticated(true);
+        setUser(response.data.user);
+      } catch (error) {
+        setIsAuthenticated(false);
+        setUser({});
+      }
+    };
+    fetchUser();
+  }, [isAuthenticated]);
+
   return (
     <>
-       <Router>
+      <Router>
+        <Navbar />
         <Routes>
-          <Route path='/' element={<Home/>}></Route>
-          <Route path='/appointment' element={<Appointment/>}></Route>
-          <Route path='/about' element={<About/>}></Route>
-          <Route path='/register' element={<Register/>}></Route>
-          <Route path='/login' element={<Login/>}></Route>
+          <Route path="/" element={<Home />} />
+          <Route path="/appointment" element={<Appointment />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
-        <ToastContainer position='top-center'/>
-       </Router>
+        <Footer />
+        <ToastContainer position="top-center" />
+      </Router>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
