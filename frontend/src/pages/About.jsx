fcedb7fea4a45ee84768e37dc743adff70e1c21a
Shubham kahar <Shubhamkahar196@gmail.com>
2025-04-22 20:55:24 +0530

    Adding about page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Hero from "../components/Hero"
+import Biography from "../components/Biography"
+
+
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+     <Hero 
+     title={"Learn More About us | ZeeCare Medical Institute"}
+     imageUrl={"/about.png"}
+     />
+     <Biography imageUrl={"/whoweare.png"}/>"
+     </>
   )
 }
 
